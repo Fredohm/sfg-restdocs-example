c1ec8519f9c174927e863e967b5b54bd0ace6e9e
Fredohm <fredwaucquez@hotmail.fr>
2021-03-21 17:32:12 +0100

    Final Commit

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-21T11:29:45+0100",
+    date = "2021-03-21T17:30:08+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.9.1 (JetBrains s.r.o.)"
 )
 @Component
